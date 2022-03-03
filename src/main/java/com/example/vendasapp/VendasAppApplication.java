48bfe0ad0021d6470b95da132d2634a74abdb7c7
anderson.sanctos <anderson_ngc@hotmail.com>
2022-03-03 07:39:48 -0300

    Primeiro commit

---
+++ src/main/java/com/example/vendasapp/VendasAppApplication.java
@@ -0,0 +1,55 @@
+package com.example.vendasapp;
+
+import domain.entity.Cliente;
+import com.example.vendasapp.domain.repositorios.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class VendasAppApplication {
+
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes){
+		return args -> {
+			System.out.println("Salvando clientes");
+			clientes.salvar(new Cliente("Douglas"));
+			clientes.salvar(new Cliente("Outro Cliente"));
+
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+//            System.out.println("Atualizando clientes");
+//            todosClientes.forEach(c -> {
+//                c.setNome(c.getNome() + " atualizado.");
+//                clientes.atualizar(c);
+//            });
+//
+//            todosClientes = clientes.obterTodos();
+//            todosClientes.forEach(System.out::println);
+//
+//            System.out.println("Buscando clientes");
+//            clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+//            System.out.println("deletando clientes");
+//            clientes.obterTodos().forEach(c -> {
+//                clientes.deletar(c);
+//            });
+
+//            todosClientes = clientes.obterTodos();
+//            if(todosClientes.isEmpty()){
+//                System.out.println("Nenhum cliente encontrado.");
+//            }else{
+//                todosClientes.forEach(System.out::println);
+//            }
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasAppApplication.class, args);
+	}
+}
